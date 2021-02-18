3fb4a997682a7bf12e65454f91595e152a5bdf7b
Eric Sutherland <ericsutherland@ou.edu>
2021-02-18 14:02:18 -0600

    google search

---
+++ WPF_Browser/WPF_Browser/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Browser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btngo_Click(object sender, RoutedEventArgs e)
+        {
+            string query = txtquery.Text;
+
+            MyWebBrowserWindow google = new MyWebBrowserWindow();
+
+            google.Query = query;
+            google.Search(query);
+            google.ShowDialog();
+
+        }
+    }
+}
---
+++ WPF_Browser/WPF_Browser/MyWebBrowserWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Browser
+{
+    /// <summary>
+    /// Interaction logic for MyWebBrowserWindow.xaml
+    /// </summary>
+    public partial class MyWebBrowserWindow : Window
+    {
+
+        public string Query { get; set; }
+
+        public MyWebBrowserWindow()
+        {
+            InitializeComponent();
+
+            webgoogle.Navigate($"https://www.google.com/search?q={Query}");
+        }
+
+        public void Search(string q)
+        {
+
+            webgoogle.Navigate($"https://www.google.com/search?q={Query}");
+        }
+    }
+}
