e9fa3f70a9c2cc71dd02b58b53c2bde9f022742f
Eric Sutherland <ericsutherland@ou.edu>
2021-02-23 14:30:02 -0600

    json1

---
+++ JSON_In_Class/JSON_In_Class/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace JSON_In_Class
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //https://rickandmortyapi.com/api/character
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://rickandmortyapi.com/api/character").Result;
+
+                RickAndMortyAPI api = JsonConvert.DeserializeObject<RickAndMortyAPI>(jsonData);
+
+                foreach (var character in api.results)
+                {
+                    lstcharacters.Items.Add(character);
+                }
+
+            }
+
+        }
+
+        private void lstcharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedcharacter = (Character)lstcharacters.SelectedItem;
+            imgcharacter.Source = new BitmapImage(new Uri(selectedcharacter.image));
+
+        }
+    }
+}
